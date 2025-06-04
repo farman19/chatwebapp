014aacf45ec5a5ddb302abd4ce502634d5fc19b4
farman19 <mohdfarman0209@gmail.com>
2025-06-04 11:18:10 +0530

    fix auth user

--- chatserver/server.js
+++ chatserver/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://chatxfrontend.onrender.com",
+ origin: ["https://chatxfrontend.onrender.com", "http://localhost:3000"],
   credentials: true
 }));
 
