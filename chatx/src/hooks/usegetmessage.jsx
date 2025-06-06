3770ff375fb2c13749333d4924fa55c1b5077390
farman19 <mohdfarman0209@gmail.com>
2025-06-06 17:38:40 +0530

    message error solve

--- chatx/src/hooks/usegetmessage.jsx
+++ chatx/src/hooks/usegetmessage.jsx
@@ -1,18 +1,19 @@
-import axios from "axios";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMessages, clearMessagesForUser } from "../redux/messageSlice";
+import axios from "axios";
 
 const useGetMessages = () => {
   const dispatch = useDispatch();
   const authUser = useSelector((store) => store.user.authUser);
   const selectedUser = useSelector((store) => store.user.selectedUser);
-  const socket = useSelector((store) => store.socket.socket); // ✅ Fix 1
+  const socket = useSelector((store) => store.socket.socket);
 
   const fetchMessages = async () => {
     if (!socket || !authUser || !selectedUser) return;
 
     try {
       axios.defaults.withCredentials = true;
+
       const res = await axios.get(
         `https://chatx-xilj.onrender.com/api/v1/message/${authUser._id}/${selectedUser._id}`
       );
@@ -20,11 +21,12 @@ const useGetMessages = () => {
       const fetchedMessages = Array.isArray(res.data.messages)
         ? res.data.messages
         : [];
-      console.log("***********=====>",fetchMessages)
+
+      console.log("***********=====>", res.data.messages); // ✅ FIXED: removed typo `fetchMessages.res`
+
       dispatch(setMessages(fetchedMessages));
 
-
-      // ✅ Fix 2: unseenMsg अब fetchedMessages से check करो
+      // ✅ Check for unseen message to emit 'message-seen'
       const unseenMsg = [...fetchedMessages].reverse().find(
         (msg) =>
           msg.senderId === selectedUser._id &&
